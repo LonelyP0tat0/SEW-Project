4554a9af526986d3f882cdc4f261f1ef58a80978
LonelyP0tat0 <luciandragann@gmail.com>
2022-05-31 13:14:52 +0200

    First GUI elements implemented + comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,13 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Player player1 = new Player("jerryTheSmol", "archer");
-        Monster skeleton = new Monster("skeleton");
-        Monster troll = new Monster("troll");
-        Monster goblin = new Monster("goblin");
-        Boss boss = new Boss("Malenia");
 
-        player1.fight(troll);
 
     }
 
